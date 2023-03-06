51251b4c5bf8f7e8539f15d31e16e9953e76498b
Brandon Naude <naudebrandon@gmail.com>
2023-03-06 21:22:27 +0200

    Update main.py
    added the loop to create the tickets, with the number formatting

--- main.py
+++ main.py
@@ -3,37 +3,37 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-img = Image.open("D:\Downloads\srcimg.jpg")
-
-# Call draw Method to add 2D graphics in an image
-# I1 = ImageDraw.Draw(img)
-
-
-# Custom font style and font size
-myFontB = ImageFont.truetype('arial.ttf', 430)
-myFontS = ImageFont.truetype('arial.ttf', 270) 
-
-# Add Text to an image
-# I1.text((100, 450), "0001", font=myFontB, fill=(0, 0, 0))
+start = 1
+end = 1000
+output = "D:\Downloads\exports\srcimg {0:04d}.jpg"
 
 
-tim = Image.new("RGBA", (650,260), (0, 0 ,0, 0))
-dr = ImageDraw.Draw(tim)
-dr.text((30,0),"0001", font=myFontS, fill=(0,0,0))
+for i in range (start,end + 1):
+        img = Image.open("D:\Downloads\srcimg.jpg")
 
-tim = tim.rotate(315, expand=1)
-img.paste(tim, (1030,30), tim)
-
-# img2 = Image.open("D:\Downloads\exports\srcimg2.jpg")
-#img = img.rotate(45,expand=1)
-#I2 = ImageDraw.Draw(img)
-#I2.text((870,340),"0001", font=myFontS, fill=(0 ,0, 0))
-
-#img = img.rotate(315 ,expand=1)
+        # Call draw Method to add 2D graphics in an image
+        I1 = ImageDraw.Draw(img)
 
 
+        # Custom font style and font size
+        myFontB = ImageFont.truetype('arial.ttf', 430)
+        myFontS = ImageFont.truetype('arial.ttf', 270) 
 
-# Save the edited image
-img.save("D:\Downloads\exports\srcimg2.jpg")
+        # Add Text to an image
+        I1.text((100, 450), "{0:04d}".format(i), font=myFontB, fill=(0, 0, 0))
+
+
+        img2 = Image.new("RGBA", (650,260), (0, 0 ,0, 0))
+        I2 = ImageDraw.Draw(img2)
+        I2.text((30,0),"{0:04d}".format(i), font=myFontS, fill=(0,0,0))
+
+        img2 = img2.rotate(315, expand=1)
+        img.paste(img2, (1030,30), img2)
+
+
+        # Save the edited image
+        img.save(output.format(i))
+
+
 
 
